32fe595acdd67124a36d5e6f5fd2dfad663e696e
Nam Tran <nhtranngoc@wpi.edu>
2020-07-21 08:46:48 -0400

    Added basic boid & vector

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -5,17 +5,37 @@
 </template>
 
 <script>
+
+import Victor from 'victor';
+
 export default {
     name: 'Canvas',
+    data() {
+        return {
+            boid: {
+                pos: null,
+                dir: null
+            }
+        }
+    },
     methods: {
         startDrawing: function() {
             window.requestAnimationFrame(this.redraw);
+            this.boid.dir = new Victor(5,3);
+            this.boid.pos = new Victor(10,10);
         },
         redraw: function() {
             let ctx = this.$refs.canvas.getContext('2d');
-            ctx.clearRect(0,0,this.$refs.canvas.width,this.$refs.canvas.height);
+            ctx.clearRect(0,0,this.width,this.height);
             // Main boid frame here
-            this.drawBoid(50,50,10,'rgb(200,0,0)');
+            this.boid.pos = this.boid.pos.add(this.boid.dir);
+            if ((this.boid.pos.x + 10) > this.width || (this.boid.pos.x - 10) < 0) {
+                this.boid.dir.x = -this.boid.dir.x;
+            }
+            if ((this.boid.pos.y + 10) > this.height || (this.boid.pos.y - 10) < 0) {
+                this.boid.dir.y = -this.boid.dir.y;
+            }
+            this.drawBoid(this.boid.pos.x,this.boid.pos.y,10,'rgb(200,0,0)');
             
             window.requestAnimationFrame(this.redraw);
         },
