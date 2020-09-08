5da5674bec1791843523b2b4267072b97934d4e1
Nam Tran <nhtranngoc@wpi.edu>
2020-09-08 11:42:57 -0400

    Fixed NavBar link to Vuetify

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,41 +1,17 @@
 <template>
-    <div class="nav">
+    <div>
         <ul>
-        <li class="nav-section"><router-link class="nav-link" to="/about" title="About me">About</router-link></li> |
-        <li class="nav-section"><router-link class="nav-link" to="/hireme" title="I'm open for hiring!">Hire Me</router-link></li> |
-        <li class="nav-section"><router-link class="nav-link" to="/projects" title="My projects">Projects</router-link></li> |
-        <li class="nav-section"><router-link class="nav-link" to="/site" title="What's all this?">What's This?</router-link></li> |
-        <li class="nav-section"><a href="https://www.dropbox.com/s/9dgllxi4trqdd29/NamTran_Resume.pdf?dl=1" title="Download my resume">Resume</a></li>
+            <li class="nav-section"><v-btn text="true" to="/about" title="About me">About</v-btn></li> |
+            <li class="nav-section"><v-btn text="true" to="/hireme" title="I'm open for hiring!">Hire Me</v-btn></li> |
+            <li class="nav-section"><v-btn text="true" to="/projects" title="My projects">Projects</v-btn></li> |
+            <li class="nav-section"><v-btn text="true" to="/site" title="What's all this?">What's This?</v-btn></li> |
+            <li class="nav-section"><v-btn text="true" href="https://www.dropbox.com/s/9dgllxi4trqdd29/NamTran_Resume.pdf?dl=1" title="Download my resume">Resume</v-btn></li>
         </ul>
     </div>
 </template>
 
 <style scoped>
-ul {
-    padding: 0;
-}
-.nav {
-    position: relative;
-    margin: 0;
-    padding: 0;
-}
-
 .nav-section {
-    position: relative;
     display: inline-block;
-    margin: 0;
-    padding: 0;
-
-}
-
-.nav-link, a{
-    padding: 0 5px;
-    text-decoration: none;
-    color: #f1faee;
-    border-radius: 5%;
-}
-
-.nav-link:hover, a:hover{
-    background-color: #457b9d;
 }
 </style>
