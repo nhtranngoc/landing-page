6d7f0d93594cc07e9467d28717c7d2fcb0f59fc4
Nam Tran <nhtranngoc@wpi.edu>
2020-09-08 14:01:27 -0400

    Refactored Navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,66 @@
 <template>
     <div>
         <ul>
-            <li class="nav-section"><v-btn text to="/about" title="About me">About</v-btn></li> |
-            <li class="nav-section"><v-btn text to="/hireme" title="I'm open for hiring!">Hire Me</v-btn></li> |
-            <li class="nav-section"><v-btn text to="/projects" title="My projects">Projects</v-btn></li> |
-            <li class="nav-section"><v-btn text to="/site" title="What's all this?">What's This?</v-btn></li> |
-            <li class="nav-section"><v-btn text href="https://www.dropbox.com/s/9dgllxi4trqdd29/NamTran_Resume.pdf?dl=1" title="Download my resume">Resume</v-btn></li>
+            <template
+            v-for="(link, index) in links"
+            class="nav-section">
+                <v-tooltip bottom :key="link.href">
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn
+                        text
+                        v-bind="attrs"
+                        v-on="on"
+                        :to="link.href">
+                        {{ link.text }}
+                        </v-btn>
+                        <v-divider 
+                        inset
+                        vertical
+                        light
+                        v-if="index < links.length - 1" />
+                    </template>
+                    <span>{{ link.tooltip }}</span>
+                </v-tooltip>
+            </template>
         </ul>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    text: 'About',
+                    tooltip: 'About me',
+                    href: '/about'
+                },
+                {
+                    text: 'Hire Me',
+                    tooltip: 'I\'m open for hiring!',
+                    href: '/hireme'
+                },
+                {
+                    text: 'Projects',
+                    tooltip: 'Things I build for fun',
+                    href: '/projects'
+                },
+                {
+                    text: 'What\'s this?',
+                    tooltip: 'About this site',
+                    href: '/site'
+                },
+                {
+                    text: 'Resume',
+                    tooltip: 'Everything packed in one neat little page',
+                    href: 'https://www.dropbox.com/s/9dgllxi4trqdd29/NamTran_Resume.pdf?dl=1'
+                }
+            ]
+        }
+    }
+}
+</script>
 
 <style scoped>
 .nav-section {
