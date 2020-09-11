1fb0a4190982235706405dd9e0ad7e548dc8f38a
Nam Tran <nhtranngoc@wpi.edu>
2020-09-11 15:44:41 -0400

    Fixed About Page layout

--- src/About.vue
+++ src/About.vue
@@ -1,17 +1,25 @@
 <template>
-    <div>
+    <v-content>
         <TopBar :nav="nav"/>
-        <img id="headshot" src="../public/headshot.jpg">
-        <div class="paragraph">
-            <p>NamFromNam is a collection of loose wires and duct tape masquerading as a website, built by an even looser collection of wires and coffee known as Nam Tran.</p>
-            <p>In my spare time, I enjoy picking up new hobbies which include cooking, baking, brewing, programming, gardening, woodworking, and crocheting.</p>
+        <v-container fill-height fluid>
+            <v-row align="center" justify="center">
+                <v-col md="auto">
+                    <v-avatar size="30vh">
+                        <v-img src="../public/headshot.jpg" />
+                    </v-avatar>
+                </v-col>
+                <v-col cols="7">
+                    <p>NamFromNam is a collection of loose wires and duct tape masquerading as a website, built by an even looser collection of wires and coffee known as Nam Tran.</p>
+                    <p>In my spare time, I enjoy picking up new hobbies which include cooking, baking, brewing, programming, gardening, woodworking, and crocheting.</p>
 
-            <h1> Contact Info </h1>
-            <p> My email is highly classified, and is in no way, shape, or form <a href="mailto:nam@namfromnam.com">nam@namfromnam.com</a></P>
-            <p> I also have a GitHub page <a href="https://github.com/nhtranngoc">@nhtranngoc</a> where I put all my silly projects.</p>
-        </div>
+                    <h1> Contact Info </h1>
+                    <p> My email is highly classified, and is in no way, shape, or form <a href="mailto:nam@namfromnam.com">nam@namfromnam.com</a></P>
+                    <p> I also have a GitHub page <a href="https://github.com/nhtranngoc">@nhtranngoc</a> where I put all my silly projects.</p>
+                </v-col>
+            </v-row>
+        </v-container>
         <router-view></router-view>
-    </div>
+    </v-content>
 </template>
 
 <script>
@@ -32,7 +40,7 @@ export default {
 </script>
 
 <style scoped>
-#headshot {
+/* #headshot {
     display: block;
     margin-top: 1em;
     margin-bottom: 2em;
@@ -41,6 +49,6 @@ export default {
     width: 12%;
     height: auto;
     border-radius: 50%;
-}
+} */
 
 </style>
